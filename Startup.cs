5a4d926842abcf59c8b3e72b2e3c41953031a6fc
Ryan Transfiguracion <ryanthtra@mvemjsunp.fios-router.home>
2016-12-20 19:37:20 -0800

    - added Newtonsoft.json via NuGet command - manually added tsconfig.json (for TypeScript compiler support) - manually added package.json (for NPM support) - package.json -> added gulp and typescript dependencies - added gulpfile.js (for adding Gulp task-running) - used npm install command to install dependencies to project

--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,14 @@ namespace OpenGameList
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            // Configure a rewrite rule to auto-lookup for standard default files such as index.html.
+            app.UseDefaultFiles();
+
+            // Serve static files (html, css, js, images & more). See also the following URL:
+            // https://docs.asp.net/en/latest/fundamentals/static-files.html for further reference
+            app.UseStaticFiles();
+
+            // Add MVC to the pipeline
             app.UseMvc();
         }
     }
