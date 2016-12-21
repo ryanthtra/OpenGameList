2f9564998c9d17183f18327f26a2db703745e115
Ryan Transfiguracion <ryanthtra@mvemjsunp.fios-router.home>
2016-12-21 12:42:46 -0800

    - added Angular 2 TS files

---
+++ Scripts/app/app.module.ts
@@ -0,0 +1,26 @@
+/// <reference path="app.component.ts./../typings/index.d.ts"/>
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpModule } from "@angular/http";
+import "rxjs/Rx";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  // Directives, components, and pipes
+  declarations: [
+    AppComponent
+  ],
+  // Modules
+  imports: [
+    BrowserModule,
+    HttpModule
+  ],
+  // Providers
+  providers: [
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
